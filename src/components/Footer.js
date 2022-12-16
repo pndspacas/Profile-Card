c6d59c54cb46d549b3c93053ba01d9238b68537c
Pedro Pacas <pndspacas@gmail.com>
2022-12-16 04:50:10 +0000

    added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,20 @@ const Footer = () => {
   return (
     <footer>
       <a href="https://github.com/pndspacas">
-        <img src="./images/GitHub.png" alt="github" />
+        <img
+          src="./images/GitHub.png"
+          alt="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
       </a>
       <a href="https://www.linkedin.com/in/pndspacas/">
-        <img src="./images/Linkedin.png" alt="linkedin" />
+        <img
+          src="./images/Linkedin.png"
+          alt="linkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
       </a>
     </footer>
   );
